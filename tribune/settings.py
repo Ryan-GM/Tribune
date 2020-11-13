35e1f5824f0bccd303864a94d60d261b14741254
Munge Ryan <mungeryan@gmail.com>
2020-11-13 09:36:26 +0300

    deploying to heroku master

--- tribune/settings.py
+++ tribune/settings.py
@@ -22,7 +22,7 @@ import cloudinary.api
 
 MODE = config("MODE",default = "dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG',default = True, cast = bool)
+DEBUG = config('DEBUG',default = False, cast = bool)
 # development
 if config('MODE') == "dev":
     DATABASES = {
